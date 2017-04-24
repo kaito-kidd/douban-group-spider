df5d7fcefee7175b37aedd4b8169d7d13a1e1e7e
root <root@Master.(none)>
2017-04-24 11:21:09 +0800

    add search entry

--- config.py
+++ config.py
@@ -15,35 +15,35 @@ DB_URI = "mongodb://127.0.0.1:27017"
 # DB_URI = "mongodb://192.168.6.7:27017"
 DB_NAME = "douban_group"
 
+# 搜索入口
+ENABLE_ENTRY = True
+ENTRY = "https://www.douban.com/group/search"
+ENTRY_MAX_PAGE = 5
+ENTRY_BLACKLIST = []
+ENTRY_CATALOG = 1019
+ENTRY_QUERY = u"家居"
+
 # 豆瓣小组URL
 GROUP_LIST = [
-    # 北京租房豆瓣
-    "http://www.douban.com/group/26926/",
-    # 北京租房（非中介）
-    "http://www.douban.com/group/279962/",
-    # 北京租房房东联盟(中介勿扰)
-    "http://www.douban.com/group/257523/",
-    # 北京租房
-    "http://www.douban.com/group/beijingzufang/",
-    # 北京租房小组
-    "http://www.douban.com/group/xiaotanzi/",
-    # 北京无中介租房
-    "http://www.douban.com/group/zhufang/",
-    # 北漂爱合租
-    "http://www.douban.com/group/aihezu/",
-    # 北京同志们来租房
-    "http://www.douban.com/group/325060/",
-    # 北京个人租房
-    "http://www.douban.com/group/opking/",
-    # 北京租房小组!
-    "http://www.douban.com/group/374051/",
+    # 爱生活！爱家居！爱艺术！
+    "http://www.douban.com/group/sheyii/",
+    # 家居党—家居知识NO.1小组
+    "http://www.douban.com/group/household/",
+    # 我爱我家（家电&家居&数码）
+    "http://www.douban.com/group/532638/",
+    # 软装配饰设计|家居软装饰设计
+    "http://www.douban.com/group/DECORATIONS/",
+    # DIY家居生活馆
+    "http://www.douban.com/group/DIY-jiaju/",
+    # 智能硬件 智能家居有多机智？
+    "http://www.douban.com/group/538230/",
 ]
 
 # 后缀
 GROUP_SUFFIX = "discussion?start=%d"
 
 # 抓取前多少页
-MAX_PAGE = 5
+MAX_PAGE = 25
 
 # 匹配规则
 RULES = {
@@ -63,6 +63,8 @@ RULES = {
     "create_time": "//span[@class='color-green']/text()",
     "detail_author": "//span[@class='from']/a/text()",
     "content": "//div[@class='topic-content']/p/text()",
+    # 搜索入口
+    "group_list": "//div[@class='title']/h3/a/@href"
 }
 
 # 并发数
